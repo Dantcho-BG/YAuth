faedb70c83f01b7078c339d86bc830ac42b0d0b1
Iordan Hristov <dantcho.bg@gmail.com>
2016-12-01 18:45:40 +0000

    First Website Upload
    Added: Header, Nav bar, Section and Footer

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>YAuth</title>
+  </head>
+  <body>
+    <header>
+      <div>YAuth</div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>NAV ITEM</li>
+          <li>NAV ITEM</li>
+        </ul>
+      </nav>
+    </header>
+    <section>
+    </section>
+    <footer>
+      <div>2016 - © All rights reserved</div>
+    </footer>
+  </body>
+</html>
