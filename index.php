3fc6d4a64985aa220436c2c84723b0be09fbb0c3
Iordan Hristov <dantcho.bg@gmail.com>
2016-12-01 22:27:37 +0000

    The header and the navbar are done

--- index.php
+++ index.php
@@ -3,15 +3,16 @@
   <head>
     <meta charset="utf-8" />
     <title>YAuth</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
   </head>
   <body>
     <header>
       <div class="website_title">YAuth</div>
       <nav>
-        <ul>
-          <li>Home</li>
-          <li>NAV ITEM</li>
-          <li>NAV ITEM</li>
+        <ul class="website_navbar">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="index.php">Page 2</a></li>
+          <li><a href="index.php">Page 3</a></li>
         </ul>
       </nav>
     </header>
