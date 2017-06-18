d773346ef14d8ba33b411272992fdfb4163d59fe
Iordan Hristov <dantcho.bg@gmail.com>
2017-06-18 19:40:19 +0100

    Live Javascript form validation

--- js/liveFormValidation.js
+++ js/liveFormValidation.js
@@ -1,64 +1,42 @@
+//Button State Changer DISABLED
+/*
+function enableButton(buttonID) {
+  var element = document.getElementById(buttonID);
+  element.disabled = false;
+}
 
-//Validators
+function disableButton(buttonID) {
+  var element = document.getElementById(buttonID);
+  element.disabled = true;
+}
+*/
 
-function validateFirstName(inputID) {
-  var firstName = document.getElementById(inputID).value;
-  if (firstName === "") {
-    return "stava";
-  }
-  else {
-    changeInputStyleToDanger(inputID);
-    return "ne stava";
+//Button Functions
+
+function submitForm (formID) {
+  document.getElementById(formID).submit();
+}
+
+//Check What The State Of The Button Shoud Be
+
+var numberOfValidInputs = 0;
+
+function checkInputValidation (numberOfInputs, formID) {
+  if (numberOfValidInputs === numberOfInputs) {
+    submitForm(formID);
   }
 }
 
-function validateMiddleName(inputID) {
-  var firstName = document.getElementById(inputID).value;
-  if (firstName === "") {
-    return "stava";
-  }
-  else {
-    return "ne stava";
-  }
+function addValidInput() {
+  return numberOfValidInputs++;
 }
 
-function validateLastName(inputID) {
-  var firstName = document.getElementById(inputID).value;
-  if (firstName === "") {
-    return "stava";
+function removeValidInput() {
+  if (numberOfValidInputs === 0) {
+    return numberOfValidInputs;
   }
   else {
-    return "ne stava";
-  }
-}
-
-function validateUsername(inputID) {
-  var firstName = document.getElementById(inputID).value;
-  if (firstName === "") {
-    return "stava";
-  }
-  else {
-    return "ne stava";
-  }
-}
-
-function validate(inputID) {
-  var firstName = document.getElementById(inputID).value;
-  if (firstName === "") {
-    return "stava";
-  }
-  else {
-    return "ne stava";
-  }
-}
-
-function validateUsername(inputID) {
-  var firstName = document.getElementById(inputID).value;
-  if (firstName === "") {
-    return "stava";
-  }
-  else {
-    return "ne stava";
+    return numberOfValidInputs--;
   }
 }
 
@@ -66,24 +44,220 @@ function validateUsername(inputID) {
 
 function changeInputStyleToSuccess(inputID) {
 
-  var element = document.getElementById(inputID);
-  element.setAttribute("id", inputID+", inputSuccess");
+  //var element = document.getElementById(inputID);
+  //element.setAttribute("id", inputID+", inputSuccess");
 
   inputID = inputID.charAt(0).toUpperCase() + inputID.slice(1);
 
   var element = document.getElementById("div"+inputID);
+
+  var a = element.className.indexOf("has-success");
+
   element.setAttribute("class", "form-group has-success");
 
+  if (a === -1) {
+    addValidInput();
+  }
+
+}
+
+function changeInputStyleToWarning(inputID) {
+
+  //var element = document.getElementById(inputID);
+  //element.setAttribute("id", inputID+", inputSuccess");
+
+  inputID = inputID.charAt(0).toUpperCase() + inputID.slice(1);
+
+  var element = document.getElementById("div"+inputID);
+  element.setAttribute("class", "form-group has-warning");
 }
 
 function changeInputStyleToDanger(inputID) {
 
-  var element = document.getElementById(inputID);
-  element.setAttribute("id", inputID+", inputSuccess");
+  //var element = document.getElementById(inputID);
+  //element.setAttribute("id", inputID+", inputSuccess");
 
   inputID = inputID.charAt(0).toUpperCase() + inputID.slice(1);
 
   var element = document.getElementById("div"+inputID);
-  element.setAttribute("class", "form-group has-success");
 
+  var a = element.className.indexOf("has-error");
+
+  element.setAttribute("class", "form-group has-error");
+
+  if (a === -1) {
+    removeValidInput();
+  }
+
+}
+
+//Validators
+
+function validateFirstName(inputID) {
+  var firstName = document.getElementById(inputID).value;
+  if (firstName === "") {
+    changeInputStyleToDanger(inputID);
+    return "First Name Required";
+  }
+  else {
+    if (firstName.length > 30) {
+      changeInputStyleToDanger(inputID);
+      return "First Name Too Long"
+    }
+    if (firstName.length < 2) {
+      changeInputStyleToDanger(inputID);
+      return "First Name Too Short";
+    }
+    if (!/^[a-zA-Z]+$/.test(firstName)){
+      changeInputStyleToDanger(inputID);
+      return "First Name Contains Unallowed Symbols";
+    }
+    changeInputStyleToSuccess(inputID);
+    return "First Name Available";
+  }
+}
+
+function validateMiddleName(inputID) {
+  var middleName = document.getElementById(inputID).value;
+  if (middleName === "") {
+    changeInputStyleToDanger(inputID);
+    return "Middle Name Required";
+  }
+  else {
+    if (middleName.length > 30) {
+      changeInputStyleToDanger(inputID);
+      return "Middle Name Too Long"
+    }
+    if (middleName.length < 2) {
+      changeInputStyleToDanger(inputID);
+      return "Middle Name Too Short";
+    }
+    if (!/^[a-zA-Z]+$/.test(middleName)){
+      changeInputStyleToDanger(inputID);
+      return "Middle Name Contains Unallowed Symbols";
+    }
+    changeInputStyleToSuccess(inputID);
+    return "Middle Name Available";
+  }
+}
+
+function validateLastName(inputID) {
+  var lastName = document.getElementById(inputID).value;
+  if (lastName === "") {
+    changeInputStyleToDanger(inputID);
+    return "Last Name Required";
+  }
+  else {
+    if (lastName.length > 30) {
+      changeInputStyleToDanger(inputID);
+      return "Last Name Too Long"
+    }
+    if (lastName.length < 2) {
+      changeInputStyleToDanger(inputID);
+      return "Last Name Too Short";
+    }
+    if (!/^[a-zA-Z]+$/.test(lastName)){
+      changeInputStyleToDanger(inputID);
+      return "Last Name Contains Unallowed Symbols";
+    }
+    changeInputStyleToSuccess(inputID);
+    return "Last Name Available";
+  }
+}
+
+function validateUsername(inputID) {
+  var username = document.getElementById(inputID).value;
+  if (username === "") {
+    changeInputStyleToDanger(inputID);
+    return "Username Required";
+  }
+  else {
+    if (username.length > 30) {
+      changeInputStyleToDanger(inputID);
+      return "Username Too Long"
+    }
+    if (username.length < 2) {
+      changeInputStyleToDanger(inputID);
+      return "Username Too Short";
+    }
+    if (!/^[a-zA-Z_0-9]+$/.test(username)){
+      changeInputStyleToDanger(inputID);
+      return "Username Contains Unallowed Symbols";
+    }
+    changeInputStyleToSuccess(inputID);
+    return "Username Available";
+  }
+}
+
+function validateEmail(inputID) {
+  var email = document.getElementById(inputID).value;
+  if (email === "") {
+    changeInputStyleToDanger(inputID);
+    return "Email Required";
+  }
+  else {
+    if (email.length > 30) {
+      changeInputStyleToDanger(inputID);
+      return "Email Too Long"
+    }
+    if (email.length < 2) {
+      changeInputStyleToDanger(inputID);
+      return "Email Too Short";
+    }
+    if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+      changeInputStyleToDanger(inputID);
+      return "Email Is Not Valid";
+    }
+    changeInputStyleToSuccess(inputID);
+    return "Email Available";
+  }
+}
+
+function validatePassword(inputID) {
+  var password = document.getElementById(inputID).value;
+  if (password === "") {
+    changeInputStyleToDanger(inputID);
+    return "Password is Required";
+  }
+  else {
+    if (password.length > 30) {
+      changeInputStyleToDanger(inputID);
+      return "Password Too Long"
+    }
+    if (password.length < 8) {
+      changeInputStyleToDanger(inputID);
+      return "Password Too Short";
+    }
+    else {
+      changeInputStyleToSuccess(inputID);
+      return "Password Available";
+    }
+  }
+}
+
+function validatePasswordConfirm(inputIDPassword, inputIDPasswordConfirm) {
+  var password = document.getElementById(inputIDPassword).value;
+  var passwordConfirm = document.getElementById(inputIDPasswordConfirm).value;
+  if (passwordConfirm === "") {
+    changeInputStyleToDanger(inputIDPasswordConfirm);
+    return "Password confirm is Required";
+  }
+  else {
+    if (passwordConfirm.length > 30) {
+      changeInputStyleToDanger(inputIDPasswordConfirm);
+      return "Password confirm Too Long"
+    }
+    if (passwordConfirm.length < 2) {
+      changeInputStyleToDanger(inputIDPasswordConfirm);
+      return "Password confirm Too Short";
+    }
+    if (password === passwordConfirm) {
+      changeInputStyleToSuccess(inputIDPasswordConfirm);
+      return "Password confirm Available";
+    }
+    else {
+      changeInputStyleToDanger(inputIDPasswordConfirm);
+      return "Password confirm Available";
+    }
+  }
 }
--- pages/pageContainers/registerPageContainer.php
+++ pages/pageContainers/registerPageContainer.php
@@ -11,192 +11,52 @@
               <div class="box-header with-border">
                 <h3 class="box-title">Register Form</h3>
               </div>
-              <form name="registerForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" class="form-horizontal">
+              <form name="registerForm" id="registerForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" class="form-horizontal">
                 <div class="box-body">
                   <div id="divInputFirstName" class="form-group">
                     <label for="inputFirstName" class="col-sm-2 control-label">First Name</label>
                     <div class="col-sm-10">
-                      <input type="text" name="firstName" value="<?php if ($_SERVER["REQUEST_METHOD"] == "POST") { echo $_POST["firstName"]; }?>" class="form-control" id="inputFirstName" placeholder="Type your first name here">
+                      <input type="text" onChange="validateFirstName('inputFirstName', 'submitButton')" name="firstName" value="<?php if ($_SERVER["REQUEST_METHOD"] == "POST") { echo $_POST["firstName"]; }?>" class="form-control" id="inputFirstName" placeholder="Type your first name here">
                     </div>
                   </div>
                   <div id="divInputLastName" class="form-group">
                     <label for="inputLastName" class="col-sm-2 control-label">Last Name</label>
                     <div class="col-sm-10">
-                      <input type="text" name="lastName" value="<?php if ($_SERVER["REQUEST_METHOD"] == "POST") { echo $_POST["lastName"]; } ?>" class="form-control" id="inputLastName" placeholder="Type your last name here">
+                      <input type="text" onChange="validateLastName('inputLastName', 'submitButton')" name="lastName" value="<?php if ($_SERVER["REQUEST_METHOD"] == "POST") { echo $_POST["lastName"]; } ?>" class="form-control" id="inputLastName" placeholder="Type your last name here">
                     </div>
                   </div>
                   <div id="divInputUsername" class="form-group">
                     <label for="inputUsername" class="col-sm-2 control-label">Username</label>
                     <div class="col-sm-10">
-                      <input type="text" name="username" value="<?php if ($_SERVER["REQUEST_METHOD"] == "POST") { echo $_POST["username"]; } ?>" class="form-control" id="inputUsername" placeholder="Type a username here">
+                      <input type="text" onChange="validateUsername('inputUsername', 'submitButton')" name="username" value="<?php if ($_SERVER["REQUEST_METHOD"] == "POST") { echo $_POST["username"]; } ?>" class="form-control" id="inputUsername" placeholder="Type a username here">
                     </div>
                   </div>
                   <div id="divInputEmail" class="form-group">
                     <label for="inputEmail" class="col-sm-2 control-label">Email</label>
                     <div class="col-sm-10">
-                      <input type="email" name="email" value="<?php if ($_SERVER["REQUEST_METHOD"] == "POST") { echo $_POST["email"]; } ?>" class="form-control" id="inputEmail" placeholder="Type your email here">
+                      <input type="email" onChange="validateEmail('inputEmail', 'submitButton')" name="email" value="<?php if ($_SERVER["REQUEST_METHOD"] == "POST") { echo $_POST["email"]; } ?>" class="form-control" id="inputEmail" placeholder="Type your email here">
                     </div>
                   </div>
                   <div id="divInputPassword" class="form-group">
                     <label for="inputPassword" class="col-sm-2 control-label">Password</label>
                     <div class="col-sm-10">
-                      <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Type the password you want to login with here">
+                      <input type="password" onChange="validatePassword('inputPassword', 'submitButton')" name="password" class="form-control" id="inputPassword" placeholder="Type the password you want to login with here">
                     </div>
                   </div>
                   <div id="divInputPasswordConfirm" class="form-group">
                     <label for="inputPasswordConfirm" class="col-sm-2 control-label">Confirm Password</label>
                     <div class="col-sm-10">
-                      <input type="password" name="confirmPassword" class="form-control" id="inputPasswordConfirm" placeholder="Type the password again here">
+                      <input type="password" onChange="validatePasswordConfirm('inputPassword', 'inputPasswordConfirm')" name="confirmPassword" class="form-control" id="inputPasswordConfirm" placeholder="Type the password again here">
                     </div>
                   </div>
                 </div>
                 <!-- /.box-body -->
                 <div class="box-footer">
-                  <a href="login.html" class="btn btn-default">Login</a>
-                  <button type="submit" class="btn btn-info pull-right">Sign up</button>
+                  <a href="login.php" class="btn btn-default">Login</a>
+                  <a onClick="checkInputValidation(6, 'registerForm')" class="btn btn-info pull-right">Sign up</a>
                 </div>
                 <!-- /.box-footer -->
               </form>
-            </div>
-            <div class="box box-default">
-              <div class="box-header with-border">
-                <h3 class="box-title">Login Form</h3>
-              </div>
-              <!-- /.box-header -->
-              <div class="box-body">
-                <form role="form">
-                  <!-- text input -->
-                  <div class="form-group">
-                    <label>Text</label>
-                    <input type="text" class="form-control" placeholder="Enter ...">
-                  </div>
-                  <div class="form-group">
-                    <label>Text Disabled</label>
-                    <input type="text" class="form-control" placeholder="Enter ..." disabled>
-                  </div>
-
-                  <!-- textarea -->
-                  <div class="form-group">
-                    <label>Textarea</label>
-                    <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
-                  </div>
-                  <div class="form-group">
-                    <label>Textarea Disabled</label>
-                    <textarea class="form-control" rows="3" placeholder="Enter ..." disabled></textarea>
-                  </div>
-
-                  <!-- input states -->
-                  <div class="form-group has-success">
-                    <label class="control-label" for="inputSuccess"><i class="fa fa-check"></i> Input with success</label>
-                    <input type="text" class="form-control" id="inputSuccess" placeholder="Enter ...">
-                    <span class="help-block">Help block with success</span>
-                  </div>
-                  <div class="form-group has-warning">
-                    <label class="control-label" for="inputWarning"><i class="fa fa-bell-o"></i> Input with
-                      warning</label>
-                    <input type="text" class="form-control" id="inputWarning" placeholder="Enter ...">
-                    <span class="help-block">Help block with warning</span>
-                  </div>
-                  <div class="form-group has-error">
-                    <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i> Input with
-                      error</label>
-                    <input type="text" class="form-control" id="inputError" placeholder="Enter ...">
-                    <span class="help-block">Help block with error</span>
-                  </div>
-
-                  <!-- checkbox -->
-                  <div class="form-group">
-                    <div class="checkbox">
-                      <label>
-                        <input type="checkbox">
-                        Checkbox 1
-                      </label>
-                    </div>
-
-                    <div class="checkbox">
-                      <label>
-                        <input type="checkbox">
-                        Checkbox 2
-                      </label>
-                    </div>
-
-                    <div class="checkbox">
-                      <label>
-                        <input type="checkbox" disabled>
-                        Checkbox disabled
-                      </label>
-                    </div>
-                  </div>
-
-                  <!-- radio -->
-                  <div class="form-group">
-                    <div class="radio">
-                      <label>
-                        <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-                        Option one is this and that&mdash;be sure to include why it's great
-                      </label>
-                    </div>
-                    <div class="radio">
-                      <label>
-                        <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-                        Option two can be something else and selecting it will deselect option one
-                      </label>
-                    </div>
-                    <div class="radio">
-                      <label>
-                        <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3" disabled>
-                        Option three is disabled
-                      </label>
-                    </div>
-                  </div>
-
-                  <!-- select -->
-                  <div class="form-group">
-                    <label>Select</label>
-                    <select class="form-control">
-                      <option>option 1</option>
-                      <option>option 2</option>
-                      <option>option 3</option>
-                      <option>option 4</option>
-                      <option>option 5</option>
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label>Select Disabled</label>
-                    <select class="form-control" disabled>
-                      <option>option 1</option>
-                      <option>option 2</option>
-                      <option>option 3</option>
-                      <option>option 4</option>
-                      <option>option 5</option>
-                    </select>
-                  </div>
-
-                  <!-- Select multiple-->
-                  <div class="form-group">
-                    <label>Select Multiple</label>
-                    <select multiple class="form-control">
-                      <option>option 1</option>
-                      <option>option 2</option>
-                      <option>option 3</option>
-                      <option>option 4</option>
-                      <option>option 5</option>
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label>Select Multiple Disabled</label>
-                    <select multiple class="form-control" disabled>
-                      <option>option 1</option>
-                      <option>option 2</option>
-                      <option>option 3</option>
-                      <option>option 4</option>
-                      <option>option 5</option>
-                    </select>
-                  </div>
-
-                </form>
-              </div>
-            <!-- /.box-body -->
             </div>
             <!-- /.box -->
           </section>
